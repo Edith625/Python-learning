dcbc132a0bdfe52facfda4745968567c417fb013
Edith625 <shijingyi403@gmail.com>
2025-10-17 15:28:56 +1100

    while循环

---
+++ Chatper1/while循环.py
@@ -0,0 +1,23 @@
+i = 1
+# while i <= 88888: # 88888
+    # print(i) #88887
+    # i = i + 1
+
+# 从1数到100
+# while i <= 100:
+    # print(i)
+    # i = i + 1
+
+# 从100数到1
+#i = 100
+#while i >=1:
+    #print(i)
+    #i = i - 1
+
+# 累加计算
+i = 1
+s = 0
+while i <= 100:
+    s = s + i
+    i = i + 1
+print(s)
